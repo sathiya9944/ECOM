c8811c806fb89c11539122dc4e3aca9f0d03c98d
sathiya9944 <sathiyajothic@gmail.com>
2026-01-02 20:53:15 +0530

    Fix image upload path for production

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `${process.env.BASE_URL}/images/${req.file.filename}`,
+    image_url: `/images/${req.file.filename}`,
   });
 });
 
