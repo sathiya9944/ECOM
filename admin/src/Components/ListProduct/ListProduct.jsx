8e24a32a5fd3b8622d0dc721a8ea6d68fb385f04
sathiya9944 <sathiyajothic@gmail.com>
2026-01-02 22:04:47 +0530

    Fix image upload path for production

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,34 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import './ListProduct.css'
-import cross_icon from '../../assets/cross_icon.png'
+import React, { useEffect, useState } from "react";
+import "./ListProduct.css";
+import cross_icon from "../../assets/cross_icon.png";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 const ListProduct = () => {
+  const [allproducts, setAllProducts] = useState([]);
 
-  const [allproducts,setAllProducts] = useState([]);
-
-  const fetchInfo =async ()=>{
+  const fetchInfo = async () => {
     await fetch(`${API_URL}/allproducts`)
-    .then((res)=>res.json())
-    .then((data)=>{setAllProducts(data)});
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        setAllProducts(data);
+      });
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchInfo();
-  },[])
+  }, []);
 
-  const remove_product = async (id)=>{
-    await fetch(`${API_URL}/removeproduct`,{
-      method:'POST',
-      headers:{
-        Accept:'application/json',
-        'Content-Type':'application/json',
+  const remove_product = async (id) => {
+    await fetch(`${API_URL}/removeproduct`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify({id})
-    })
+      body: JSON.stringify({ id }),
+    });
     await fetchInfo();
-  }
+  };
 
   return (
-    <div className='list-product'>
+    <div className="list-product">
       <h1>All Products List</h1>
       <div className="listproduct-format-main">
         <p>Products</p>
@@ -40,22 +44,39 @@ const ListProduct = () => {
       </div>
       <div className="listproduct-allproducts">
         <hr />
-        {allproducts.map((product,index)=>{
-          return <>
-            <div key={index} className="listproduct-format-main listproduct-format">
-              <img src={product.image} alt="" className="listproduct-product-icon" />
-              <p>{product.name}</p>
-              <p>${product.old_price}</p>
-              <p>${product.new_price}</p>
-              <p>{product.category}</p>
-              <img onClick={()=>{remove_product(product.id)}} src={cross_icon} alt="" className="listproduct-remove-icon" />
-          </div>
-          <hr />
-          </>
+        {allproducts.map((product, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                className="listproduct-format-main listproduct-format"
+              >
+                <img
+                  src={`${API_URL}${product.image}`}
+                  alt={product.name}
+                  className="listproduct-product-icon"
+                />
+
+                <p>{product.name}</p>
+                <p>${product.old_price}</p>
+                <p>${product.new_price}</p>
+                <p>{product.category}</p>
+                <img
+                  onClick={() => {
+                    remove_product(product.id);
+                  }}
+                  src={cross_icon}
+                  alt=""
+                  className="listproduct-remove-icon"
+                />
+              </div>
+              <hr />
+            </>
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListProduct
+export default ListProduct;
